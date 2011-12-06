67219a86d80253070b4b6f12953a8fbe0dd1dbea
Carl Douglas <carl.douglas@gmail.com>
2011-12-06 22:42:44 +1100

    a very basic redis client library

---
+++ redis-client.scm
@@ -0,0 +1,36 @@
+; chicken-scheme redis-client
+; Copyright (c) 2011 A. Carl Douglas
+(use socket)
+
+(define (format-command command args)
+  (sprintf "*~A\r\n$~A\r\n~A\r\n~A" 
+            (+ 1 (length args))
+            (string-length (symbol->string command))
+            (symbol->string command)
+            (apply
+              string-append 
+              (map (lambda(arg)
+                     (sprintf "$~A\r\n~A\r\n" (string-length arg) arg)) args))))
+
+(define (format-response reply)
+  (string-split reply "\r\n"))
+
+(define (make-redis-client host port)
+  (define local-socket 
+    (socket-connect/ai (address-information host port family: af/inet)))
+  (define (xfer command args)
+    (begin
+      (socket-send local-socket (format-command command args))
+      (format-response (socket-receive local-socket 4096))))
+  (lambda (command . args)
+    (case command
+      ((close) (socket-close local-socket))
+      (else (xfer command args)))))
+
+(define test-client (make-redis-client "127.0.0.1" 6379))
+(pp (test-client 'ping))
+(pp (test-client 'keys "*"))
+(pp (test-client 'lpush "scheme-test" "1234"))
+(pp (test-client 'rpop "scheme-test"))
+(test-client 'close)
+
