be8bdcf887062822f34bc9759d6f2dd76a4b9231
Carl Douglas <carl.douglas@gmail.com>
2011-12-20 23:49:31 +1100

    Use socket i/o ports instead of string buffers.

--- redis-client.scm
+++ redis-client.scm
@@ -2,45 +2,43 @@
 ; Copyright (c) 2011 A. Carl Douglas
 (use socket)
 
-(define (format-command command args)
-  (sprintf "*~A\r\n$~A\r\n~A\r\n~A" 
+(define (write-command port command args)
+  (fprintf port "*~A\r\n$~A\r\n~A\r\n~A~!" 
             (+ 1 (length args))
             (string-length (symbol->string command))
             (symbol->string command)
-            (apply
-              string-append 
+            (apply string-append 
               (map (lambda(arg)
                      (sprintf "$~A\r\n~A\r\n" (string-length arg) arg)) args))))
 
-(define (format-response reply)
-  (with-input-from-string reply 
-    (lambda()
-      (letrec ((parse (lambda(argc args)
-                  (let ((ch (read-char)))
-                    (if (or (null? ch) (eof-object? ch))
-                      args
-                      (case ch
-                        ((#\+) (parse argc (append args (list (read-line)))))
-                        ((#\*) (parse (read-line) args))
-                        ((#\:) (parse argc (append args (list (read-line)))))
-                        ((#\$) (let ((l (read-line)))
-                                 (if (> 0 (string->number l))
-                                   #f
-                                   (parse argc (append args (list (read-string (string->number l))))))))
-                        ((#\return) (parse argc args))
-                        ((#\newline) (parse argc args))
-                        (else (error "unrecognised prefix" ch (read-line)))))))))
-               (parse 0 '())))))
+(define (read-command port)
+  (letrec ((parse (lambda(argc args)
+             (if (= argc 0)
+               args
+               (let ((ch (read-char port)))
+                 (case ch
+                   ((#\+) (list (read-line port)))
+                   ((#\*) (parse (string->number (read-line port)) args))
+                   ((#\$) (parse (- argc 1)
+                            (append args 
+                              (list (read-string (string->number (read-line port)) port)))))
+                   ((#\:) (list (read-line port)))
+                   ((#\return)  (parse argc args))
+                   ((#\newline) (parse argc args))
+                   (else  (error "unrecognised prefix" ch (read-line port)))))))))
+            (parse 1 '())))
 
 (define (make-redis-client host port)
-  (define local-socket 
-    (socket-connect/ai (address-information host port family: af/inet)))
-  (define (xfer command args)
-    (begin
-      (socket-send local-socket (format-command command args))
-      (format-response (socket-receive local-socket 1048576))))
-  (lambda (command . args)
-    (case command
-      ((close) (socket-close local-socket))
-      (else (xfer command args)))))
+  (parameterize ((socket-receive-buffer-size 4096))
+    (define redis-socket 
+      (socket-connect/ai (address-information host port family: af/inet)))
+    (define-values (i o) (socket-i/o-ports redis-socket))
+    (define (xfer command args)
+      (begin
+        (write-command o command args)
+        (read-command i)))
+    (lambda (command . args)
+      (case command
+        ((close) (socket-close redis-socket))
+        (else (xfer command args))))))
 
