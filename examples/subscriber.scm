949e9d22e4afbb449b746693f763a719ca5daa9c
Carl Douglas <carl.douglas@gmail.com>
2012-04-24 20:03:54 +1000

    Example of a channel subscriber.

---
+++ examples/subscriber.scm
@@ -0,0 +1,16 @@
+; Usage:
+;   csi subscriber.scm my-channel
+(use srfi-1 redis-client)
+
+(redis-connect "127.0.0.1" 6379)
+(redis-subscribe (last (command-line-arguments)))
+
+(define (run-loop thunk)
+  (let ((response (redis-read-response (*redis-in-port*))))
+     (thunk response))
+  (run-loop thunk))
+
+(run-loop 
+  (lambda(r)
+    (printf "->~S~%" r)))
+
